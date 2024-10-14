941275314b53e36f064358b9600fb233a936d9ef
unknown <liamkyleyasona@gmail.com>
2024-10-14 23:05:17 +0800

    first comment

---
+++ highest_number_tracker.py
@@ -0,0 +1,8 @@
+# User may input 5 numbers
+# Find and print the highest number
+# Compose var1 to var 5
+# If var 1 is greater than all variables
+    # compare var 1 to var 2
+    # if var 1 is greater than var 2
+    # compare it to other variables
+    # if highest var is found, print the var
