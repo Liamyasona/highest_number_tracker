67edbfa604f463528348e6c3e437af048e53fe93
unknown <liamkyleyasona@gmail.com>
2024-10-18 01:51:00 +0800

    if statements

--- highest_number_tracker.py
+++ highest_number_tracker.py
@@ -1,13 +1,19 @@
 # Program to ask user to input 5 numbers and will print the highest number
 
-def number(var1, var2, var3):
+def highest_number(var1, var2, var3):
     highest = None
 
     # Ask user to input 5 numbers 
     for i in range(5):
-        num = float(input(f"Enter number {i + 1}: "))
-        
-# Find and print the highest number
+        user_number = float(input(f"Enter number {i + 1}: "))
+
+        # Find highest number
+        if highest is None or user_number > highest:
+            highest = user_number
+    
+    # Print the highest number
+    print(f"The highest number is: {highest}")
+    
 # Compose var1 to var 5
 # If var 1 is greater than all variables
     # compare var 1 to var 2
