431a547bd37afe7be4f54766ac38066a185befb2
unknown <liamkyleyasona@gmail.com>
2024-10-18 02:35:51 +0800

    last commit and code is working

--- highest_number_tracker.py
+++ highest_number_tracker.py
@@ -1,22 +1,21 @@
-# Program to ask user to input 5 numbers and will print the highest number
+# Program to find the highest number among five user-inputted numbers
 
-def highest_number(var1, var2, var3):
+def highest_number():
+    # Store the highest number with a variable
     highest = None
 
-    # Ask user to input 5 numbers 
+    # Ask user to input 5 numbers
+    # loop if statements using range to get 5 numbers from user
     for i in range(5):
-        user_number = float(input(f"Enter number {i + 1}: "))
+        input_number = float(input(f"Please enter any number {i + 1}: "))
+        
+        # Update the highest number among the numbers using if statements
+        if highest is None or input_number > highest:
+            highest = input_number
 
-        # Find highest number
-        if highest is None or user_number > highest:
-            highest = user_number
-    
     # Print the highest number
     print(f"The highest number is: {highest}")
-    
-# Compose var1 to var 5
-# If var 1 is greater than all variables
-    # compare var 1 to var 2
-    # if var 1 is greater than var 2
-    # compare it to other variables
-    # if highest var is found, print 
+
+# Execute the main function
+if __name__ == "__main__":
+    highest_number()
