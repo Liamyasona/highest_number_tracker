c95bd8981cc30787f2adfcae809ac29217664eff
unknown <liamkyleyasona@gmail.com>
2024-10-18 01:22:59 +0800

    functions and variable

--- highest_number_tracker.py
+++ highest_number_tracker.py
@@ -1,8 +1,16 @@
-# User may input 5 numbers
+# Program to ask user to input 5 numbers and will print the highest number
+
+def number(var1, var2, var3):
+    highest = None
+
+    # Ask user to input 5 numbers 
+    for i in range(5):
+        num = float(input(f"Enter number {i + 1}: "))
+        
 # Find and print the highest number
 # Compose var1 to var 5
 # If var 1 is greater than all variables
     # compare var 1 to var 2
     # if var 1 is greater than var 2
     # compare it to other variables
-    # if highest var is found, print the var
+    # if highest var is found, print 
